37b46a19cc5948a4840fbf625e599a2bbb277d03
Bogdan Ratiu <ratiubogdan@yahoo.com>
2018-04-28 15:30:39 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    print("hellow world")
+    a = input("cum te cheama?")
+    print("salut " + a)
+    b = input("read a number")
+    b = int(b)
+    square = b*b
+    print("patratul este ", square)
+
+main()
